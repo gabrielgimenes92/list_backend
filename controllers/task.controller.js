461817a518325672ef1e29d5a383c3c50ec6e2e3
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-06-28 15:30:41 -0700

    Adds method for checking if task is or isn't already completed, and calls endpoint to change status of task

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -1,92 +1,104 @@
-const Task = require('../models/task.model')
+const Task = require('../models/task.model');
 
 const getTasks = async (req, res) => {
-    try {
+  try {
+    let checkedTasks = [];
     const tasks = await Task.find({});
-    res.status(200).json(tasks);
-} catch (error) {
-    res.status(500).json({message: error.message})
-}}
+    for (let i = 0; i < tasks.length; i++) {
+      checkedTasks.push(tasks[i]);
+
+      if (tasks[i].completedAt) {
+        checkedTasks[i].checked = 'true';
+      } else {
+        checkedTasks[i].checked = 'false';
+      }
+    }
+    res.status(200).json(checkedTasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const getSingleTask = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const task = await Task.findById(id);
-        res.status(200).json(task);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-}
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const createTask = async (req, res) => {
-    try {
-        const task = await Task.create(req.body);
-        res.status(200).json(task)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-}
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const editTask = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const updatetask = await Task.findByIdAndUpdate(id, req.body);
+  try {
+    const { id } = req.params;
+    const updatetask = await Task.findByIdAndUpdate(id, req.body);
 
-        if (!updatetask) {
-            return res.status(404).json({message: "Task not found"})
-        } else {
-            const task = await Task.findById(id);
-            res.status(200).json(task)
-        }
-    } catch (error) {
-        res.status(500).json({message: error.message})
+    if (!updatetask) {
+      return res.status(404).json({ message: 'Task not found' });
+    } else {
+      const task = await Task.findById(id);
+      res.status(200).json(task);
     }
-}
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const deleteTask = async (req, res) => {
-    try {
-        const {id} = req.params;
-        const task = await Task.findByIdAndDelete(id)
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndDelete(id);
 
-        if (!task) {
-            return res.status(404).json({ message: "Task not found"})
-        } else {
-            res.status(200).json({ message: "task deleted successfully"})
-        }
-
-    } catch (error) {
-}}
+    if (!task) {
+      return res.status(404).json({ message: 'Task not found' });
+    } else {
+      res.status(200).json({ message: 'task deleted successfully' });
+    }
+  } catch (error) {}
+};
 
 const setTaskCompleted = async (req, res) => {
-    try {
-        const {id} = req.params;
-        let currentDate = {
-            completedAt: new Date()
-        }
-        const tasks = await Task.findByIdAndUpdate(id, currentDate);
-        res.status(200).json(tasks);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-}}
+  try {
+    const { id } = req.params;
+    let currentDate = {
+      completedAt: new Date(),
+    };
+    const tasks = await Task.findByIdAndUpdate(id, currentDate);
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 const unsetTaskCompleted = async (req, res) => {
-    try {
-        const {id} = req.params;
-        let currentDate = {
-            completedAt: null
-        }
-        const tasks = await Task.findByIdAndUpdate(id, currentDate);
-        res.status(200).json(tasks);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-}}
+  try {
+    const { id } = req.params;
+    let currentDate = {
+      completedAt: null,
+    };
+    const tasks = await Task.findByIdAndUpdate(id, currentDate);
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 module.exports = {
-    getTasks,
-    getSingleTask,
-    createTask,
-    editTask,
-    deleteTask,
-    setTaskCompleted,
-    unsetTaskCompleted
-}
+  getTasks,
+  getSingleTask,
+  createTask,
+  editTask,
+  deleteTask,
+  setTaskCompleted,
+  unsetTaskCompleted,
+};
--- models/task.model.js
+++ models/task.model.js
@@ -3,20 +3,24 @@ const mongoose = require('mongoose');
 const TaskSchema = mongoose.Schema(
   {
     description: {
-        type: String,
-        required: [true, "Please enter a task"]
+      type: String,
+      required: [true, 'Please enter a task'],
     },
     completedAt: {
       type: Date,
       required: false,
-      nullable: true
-    }
+      nullable: true,
+    },
+    checked: {
+      type: Boolean,
+      required: false,
+    },
   },
   {
     timestamps: true,
   }
 );
 
-const Task = mongoose.model("Task", TaskSchema);
+const Task = mongoose.model('Task', TaskSchema);
 
 module.exports = Task;
