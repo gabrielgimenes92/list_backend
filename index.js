01c3c0d3f13b659c7662c41e9fb33c34a66495fb
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2024-06-28 16:17:01 -0700

    Moves mongoDB string uri to .env

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
+require('dotenv').config();
+const uri = process.env.MONGO_URI;
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const taskRoute = require('./routes/task.route.js');
 
-const app = express()
+const app = express();
 
 // Middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // Routes
-app.use("/api/tasks", taskRoute)
+app.use('/api/tasks', taskRoute);
 
-mongoose.connect('mongodb+srv://gabrielcgimenes:rrpCFvvb1dnzjaVc@backenddb.iyb5wkq.mongodb.net/listDB?retryWrites=true&w=majority&appName=BackendDB')
-.then(() => {
-    console.log("Connected to database!");
+mongoose
+  .connect(uri)
+  .then(() => {
+    console.log('Connected to database!');
     app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    })
-})
-.catch(() => {
-    console.log("Connection failed!");
-})
-
+      console.log('Server is running on port 3000');
+    });
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.get('/', (req, res) => {
-    res.send("Hello from node API")
-})
+  res.send('Hello from node API');
+});
